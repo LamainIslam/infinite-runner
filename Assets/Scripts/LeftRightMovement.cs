26709de90589fedccc2dad42705d30ba5b1c27ec
LamainIslam <Lamainislam@gmail.com>
2021-09-11 13:11:12 +0100

    setup
    -basic setup
    -left right movement

---
+++ Assets/Scripts/LeftRightMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRightMovement : MonoBehaviour
+{
+    public float rotateBy;
+    private float rotated;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            transform.Rotate(0, 0, rotateBy, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, -rotateBy, Space.Self);
+        }
+    }
+}
