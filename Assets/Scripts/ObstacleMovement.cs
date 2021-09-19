73e86f92bac20c7ea3a16c8447aff8910dc6fc26
LamainIslam <Lamainislam@gmail.com>
2021-09-19 13:09:30 +0100

    pause menu

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -6,16 +6,19 @@ public class ObstacleMovement : MonoBehaviour
 {
     public float rotateBy;
     public float moveBy;
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(0, 0, rotateBy, Space.Self);
+            transform.Rotate(0, 0, rotateBy*Time.deltaTime, Space.Self);
         }
         if ( Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(0, 0, -rotateBy, Space.Self);
+            transform.Rotate(0, 0, -rotateBy * Time.deltaTime, Space.Self);
         }
-        transform.Translate(0,0, -moveBy);
+        transform.Translate(0,0, -moveBy * Time.deltaTime);
     }
 }
+
+//FixedUpdate and Time.deltaTime take time into consideration
+//Time.deltaTime also make the make fair
---
+++ Assets/Scripts/stateManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//to be able to change the scenes 
+using UnityEngine.UI;
+
+public class stateManager : MonoBehaviour
+{
+    public static bool GameIsPaused = false;//so the game knows if we are pausing or resumeing
+    public GameObject pauseMenuUI;
+    //public GameObject pauseBNT;
+    //public Text pauseBNT;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            /*if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }*/
+            if (Input.GetKeyDown(KeyCode.Escape)) 
+            { 
+                TogglePause();
+//                pauseBNT.text = GameIsPaused ? "X" : "Y";
+            }
+        }
+    }
+
+    //new code allows to make a pause button in the game UI
+    public void TogglePause()
+    {
+        GameIsPaused = !GameIsPaused; // invert
+        pauseMenuUI.SetActive(GameIsPaused);
+        Time.timeScale = GameIsPaused ? 0f : 1f;
+        // this is like if (GameIsPaused) ? this value : otherwise this value;
+    }
+    /*public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }*/
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();//close the game
+    }
+
+}
